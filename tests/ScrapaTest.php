350327c835e5a6e544d79785ad0a6d6014c047d8
andela-fokosun <okosunuzflorence@gmail.com>
2016-04-17 00:57:17 +0100

    test scrapa methods

--- tests/ScrapaTest.php
+++ tests/ScrapaTest.php
@@ -4,12 +4,28 @@ use Florence\Scrapa;
  
 class ScrapaTest extends PHPUnit_Framework_TestCase {
 
-	public function testNachHasCheese()
+	public function setUp()
 	{
-		$url = 'https://www.youtube.com/user/RihannaVEVO/about';
-
-		$webpage = new Scrapa($url);
-
-		$this->assertEquals($url, $webpage->url);
+		$this->url = 'https://www.youtube.com/user/RihannaVEVO/about';
+		$this->scrap = new Scrapa($this->url);
 	}
+
+	public function testTargetUrlIsSame()
+	{
+		$this->assertEquals($this->url, $this->scrap->url);
+	}
+
+	public function testToStringScrapDOM()
+    {
+    	$query = '//ul[@class="about-custom-links"]//a[@class="about-channel-link "]/@href';
+
+        $this->assertInternalType('string', $this->scrap->toStringScrapDOM($query));
+    }
+
+    public function testToArrayScrapDOM()
+    {
+    	$query = '//ul[@class="about-custom-links"]//a[@class="about-channel-link "]/@href';
+
+        $this->assertInternalType('array', $this->scrap->toArrayScrapDOM($query));
+    }
 }
