e678103e8a286ff581fb419c8f5949b713bffae0
andela-fokosun <okosunuzflorence@gmail.com>
2016-04-17 01:11:12 +0100

    Chore

--- example.php
+++ example.php
@@ -7,12 +7,9 @@ use Florence\Scrapa;
 
 // run the scrapper and fetch results
 
-$webpage = new Scrapa('https://www.youtube.com/user/RihannaVEVO/about');
+// $webpage = new Scrapa('https://www.youtube.com/user/RihannaVEVO/about');
 
-$webpage->scrapDOM('//ul[@class="about-custom-links"]//a[@class="about-channel-link "]/@href');
+// $webpage->scrapDOM('//ul[@class="about-custom-links"]//a[@class="about-channel-link "]/@href');
 
 // print_r($webpage->toStringScrapDOM('//ul[@class="about-custom-links"]//a[@class="about-channel-link "]/@href'));
-print_r($webpage->toArrayScrapDOM('//ul[@class="about-custom-links"]//a[@class="about-channel-link "]/@href'));
-
-
-echo phpinfo();
+// print_r($webpage->toArrayScrapDOM('//ul[@class="about-custom-links"]//a[@class="about-channel-link "]/@href'));
